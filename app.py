09df4d4dcd38fc958197387fd0e5b352005b6efe
osposto <osposto@gmail.com>
2023-11-19 20:53:13 -0300

    main

--- app.py
+++ app.py
@@ -1,29 +1,45 @@
-from flask import Flask, request, jsonify
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 import requests
 
-app = Flask(__name__)
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        payload = json.loads(post_data)
 
-@app.route('/api/chat', methods=['POST'])
-def chat():
-    input_text = request.json.get('input')
+        input_text = payload.get('input')
 
-    # Realizar la solicitud a la API de OpenAI
-    response = requests.post(
-        'https://api.openai.com/v1/chat/completions',
-        headers={
-            'Authorization': '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',
-            'Content-Type': 'application/json',
-        },
-        json={
-            'messages': [{'role': 'system', 'content': 'You are a helpful assistant.'},
-                         {'role': 'user', 'content': input_text}]
-        }
-    )
+        # Realizar la solicitud a la API de OpenAI
+        response = requests.post(
+            'https://api.openai.com/v1/chat/completions',
+            headers={
+                'Authorization': 'Bearer tu_clave_de_api',  # Reemplaza con tu clave de API
+                'Content-Type': 'application/json',
+            },
+            json={
+                'messages': [
+                    {'role': 'system', 'content': 'You are a helpful assistant.'},
+                    {'role': 'user', 'content': input_text}
+                ]
+            }
+        )
 
-    data = response.json()
-    assistant_reply = data['choices'][0]['message']['content']
+        data = response.json()
 
-    return jsonify({'message': assistant_reply})
+        # Enviar la respuesta al cliente
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps({'message': data['choices'][0]['message']['content']}).encode())
+
+def run_server():
+    port = 8000
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print(f'Starting server on port {port}')
+    httpd.serve_forever()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    run_server()
