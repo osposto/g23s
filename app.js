df193d83d128c21f14f15b066fda83845d7cbac4
osposto <osposto@gmail.com>
2023-11-20 10:26:31 -0300

    main

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-async function sendMessage() {
-    const userInput = document.getElementById('user-input').value;
+        async function sendMessage() {
+            const userInput = document.getElementById('user-input').value;
 
-    // Realizar la solicitud a la API de OpenAI
-    const response = await fetch('https://api.openai.com/v1/chat/completions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': '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',  // Reemplaza con tu clave de API
-        },
-        body: JSON.stringify({
-            'messages': [
-                {'role': 'system', 'content': 'You are a helpful assistant.'},
-                {'role': 'user', 'content': userInput}
-            ]
-        })
-    });
+            // Realizar la solicitud a la función sin servidor
+            const response = await fetch('/.netlify/functions/chat', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ input: userInput }),
+            });
 
-    const data = await response.json();
+            const data = await response.json();
 
-    // Actualizar la interfaz de usuario con la respuesta
-    const chatContainer = document.getElementById('chat-container');
-    chatContainer.innerHTML += `<div>${data.choices[0].message.content}</div>`;
-}
+            // Actualizar la interfaz de usuario con la respuesta
+            const chatContainer = document.getElementById('chat-container');
+            chatContainer.innerHTML += `<div>${data.message}</div>`;
+        }
+    
---
+++ functions/chat.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  try {
+    const response = await axios.post(
+      'https://api.openai.com/v1/chat/completions',
+      {
+        messages: [
+          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'user', content: event.body },
+        ],
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+      }
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: response.data.choices[0].message.content }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal Server Error' }),
+    };
+  }
+};
